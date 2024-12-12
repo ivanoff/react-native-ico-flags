15adef39bf868cdbb94d0fa8a9988a7d71d316e7
Dimitry Ivanov <2@ivanoff.org.ua>
2024-12-12 18:37:21 +0200

    add Syria flag

--- src/data.js
+++ src/data.js
@@ -373,16 +373,7 @@ module.exports = {
 <polygon style="fill:#FFDA44;" points="192,182.857 165.849,195.158 179.774,220.485 151.377,215.052 147.779,243.739 128,222.641   108.22,243.739 104.623,215.052 76.226,220.484 90.151,195.158 64,182.857 90.151,170.556 76.226,145.23 104.622,150.662   108.221,121.975 128,143.074 147.78,121.975 151.377,150.662 179.775,145.23 165.849,170.557 "/>
 </svg>
 `,
-	'syria': `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
-<rect y="85.331" style="fill:#F0F0F0;" width="512" height="341.337"/>
-<rect y="85.331" style="fill:#D80027;" width="512" height="113.775"/>
-<rect y="312.882" width="512" height="113.775"/>
-<g>
-	<polygon style="fill:#6DA544;" points="187.31,215.184 196.518,243.525 226.32,243.525 202.21,261.043 211.419,289.385    187.31,271.869 163.2,289.385 172.409,261.043 148.3,243.525 178.101,243.525  "/>
-	<polygon style="fill:#6DA544;" points="324.69,215.184 333.899,243.525 363.7,243.525 339.591,261.043 348.8,289.385    324.69,271.869 300.581,289.385 309.79,261.043 285.68,243.525 315.482,243.525  "/>
-</g>
-</svg>
-`,
+	'syria': `<svg xmlns="http://www.w3.org/2000/svg" width="1200" height="600"><g stroke-width="150"><path d="M0 0v600h1200V0z" fill="#007a3d"/><path d="M0 200v400h1200V200z" fill="#fff"/><path d="M0 400v200h1200V400z"/></g><g fill="#ce1126" stroke-width=".799"><path d="M600.01 228l46.677 144-122.207-88.996h151.04L553.313 372l46.678-144z"/><path d="M300.01 228l46.677 144-122.207-88.996h151.04L253.313 372l46.678-144z"/><path d="M900.01 228l46.677 144-122.207-88.996h151.04L853.313 372l46.678-144z"/></g></svg>`,
 	'guatemala': `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
 <rect y="85.331" style="fill:#F0F0F0;" width="512" height="341.337"/>
 <g>
